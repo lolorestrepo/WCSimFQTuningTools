30f4cece957ab9e1be95dd40e5efc274829d09d8
gondiaz <gdiazlop@cca018.in2p3.fr>
2023-08-03 17:22:41 +0200

    Update charge pdf tools

--- Charge/fit_charge_pdfs.py
+++ Charge/fit_charge_pdfs.py
@@ -5,7 +5,31 @@ import ROOT
 import numpy  as np
 import matplotlib.pyplot as plt
 
-from os.path   import expandvars
+from scipy.optimize import curve_fit
+
+from os.path   import expandvars, join, exists
+
+
+def get_fitting_function(mulow, muup):
+    def func(mu, *pars):
+        npars = len(pars)
+        if (mulow<=mu) & (mu<=muup):
+            return np.poly1d(np.flip(pars))(mu)
+        elif (mu<mulow):
+            c0 = sum([        pars[i]*mulow**i        for i in range(0, npars)])
+            c1 = sum([i*      pars[i]*mulow**(i-1)    for i in range(1, npars)])
+            c2 = sum([i*(i-1)*pars[i]*mulow**(i-2)/2. for i in range(2, npars)])
+            if (c2>0.): c2 = 0
+            dmu = (mu-mulow)
+            return c0 + dmu*(c1 + c2*dmu)
+        elif (muup<mu):
+            c0 = sum([        pars[i]*muup**i        for i in range(0, npars)])
+            c1 = sum([i*      pars[i]*muup**(i-1)    for i in range(1, npars)])
+            c2 = sum([i*(i-1)*pars[i]*muup**(i-2)/2. for i in range(2, npars)])
+            if (c2>0.): c2 = 0
+            dmu = (mu-muup)
+            return c0 + dmu*(c1 + c2*dmu)
+    return np.vectorize(func)
 
 
 def main():
@@ -16,8 +40,11 @@ def main():
                                     , epilog      = """""")
     
     parser.add_argument("-v", "--verbose", action="store_true")
-    parser.add_argument(   "--infile",   type=str, nargs="?", help = "charge 2D file", default="charge2D_and_unhit.root")
+    parser.add_argument(  "--infile",   type=str, nargs="?", help = "charge 2D file", default="charge2D_and_unhit.root")
     parser.add_argument( "--wcsimlib",   type=str, nargs="?", help = "WCSim lib path", default="$HOME/Software/WCSim/install/lib")
+
+    parser.add_argument(   "--npars", type=int  , nargs="+", help = "number of parameters per range", default=[4, 6, 4])
+    parser.add_argument( "--qranges", type=float, nargs="+", help = "range limits for q", default=[0., 1.45, 29.5, 1000.])
     
     args = parser.parse_args()
     ##########################################
@@ -26,25 +53,24 @@ def main():
     ROOT.gSystem.AddDynamicPath(expandvars(args.wcsimlib))
     ROOT.gSystem.Load          ("libWCSimRoot.dylib" if sys.platform == "darwin" else "libWCSimRoot.so")
 
+    # Config variables
+    infilename = args.infile
+    npars      = args.npars
+    qranges    = args.qranges
+
+    nqranges = len(npars)
+    assert nqranges == len(qranges) - 1
+
+    if args.verbose:
+        print("-------------------------")
+        print(f"Using {len(npars)} q ranges")
+        print("-------------------------")
+        for i, n in enumerate(npars): print(f"Range {i} between [{qranges[i]}, {qranges[i+1]}] with {n} parameters")
+
     # in what follows, "mu" stands for predicted and "q" for measured charge
 
-    infilename = args.infile
-    npars      = [4] # args.npars 
-    qranges    = 0   # args.qranges
-
-    # assert len(npars) == (len(qranges) + 1)
-    nqranges   = len(npars)
-
-    qranges = [0., 1.45, 29.5, 1000.]
-    npars   = [4, 6, 4]
-    nqranges = len(npars)
-
     # read 2D histogram
-    with uproot.open(infilename) as file: 
-        Hq, mubins, qbins = file["charge2D"].to_numpy()
-
-    # hardcoded
-    # qranges = np.linspace(qbins[0], qbins[-1], nqranges)
+    with uproot.open(infilename) as file: Hq, mubins, qbins = file["charge2D"].to_numpy()
 
     # compute bin centers
     mus = (mubins[1:] + mubins[:-1])/2. 
@@ -56,18 +82,16 @@ def main():
 
     # define q fit ranges and save to output
     hCPDFrange = ROOT.TH1D("hCPDFrange_type0", "", nqranges, np.array(qranges))
-    for i, n in enumerate(npars): hCPDFrange.SetBinContent(i, n)
+    for i, n in enumerate(npars, 1): hCPDFrange.SetBinContent(i, n)
     fout.WriteObject(hCPDFrange, "hCPDFrange_type0")
 
-    # output file
-    fout = ROOT.TFile("fitted_cpdf.root", "RECREATE")
-
-    # define output graphs:
+    # loop in q ranges
     # - gParams: for each range and parameter in this range: q vs parameter
     # - gmuthrs : for each range and parameter in this range: q vs low and q vs high thresholds
-    globalpi = 1
+    globalpi = 1 # global index of the qbin (unlike in range index): represents the qbin index
     for rang in range(nqranges):
-        if args.verbose: 
+        if args.verbose:
+            print()
             print(f"Processing range {rang}")
             print(f"-----------------------")
 
@@ -75,46 +99,43 @@ def main():
         qmin, qmax = qranges[rang], qranges[rang+1]
         selqbins = np.argwhere((qmin <= qs) & (qs < qmax)).flatten()
         
-        # get polynomia degree
-        deg = npars[rang]-1
-
         # graphs to fill for this range
-        gParams = [ROOT.TGraph() for par in range(npars[rang])] # one graph per parameter
+        npar = npars[rang]
+        gParams = [ROOT.TGraph() for par in range(npar)] # one graph per parameter
         gmuthrs = [ROOT.TGraph() for i in range(2)] # lower and upper bounds
 
         # fit for each q in range
         for pi, qindex in enumerate(selqbins, 0): # pi stands for point-index
             # get the qvalue, define and fill thresholds
             q = qs[qindex]
+            if args.verbose: print(f"Processing bin {globalpi}, q = {q}")
             # get pdf values for this q
-            pdf    = Hq[:, qindex]
+            pdf = Hq[:, qindex]
             # remove zeros (to avoid inf when doing the log)
             sel = ~(pdf == 0)
             pdf  = pdf[sel]
             mus_ = mus[sel]
-            # fit log(pdf) instead of pdf
-            pdflog = np.log(pdf)
+            # log(pdf) instead of pdf is fitted
+            logpdf = np.log(pdf)
 
-            # if args.verbose: print(f"Processing bin {pi+1}, q = {q}")
-            if args.verbose: print(f"Processing bin {globalpi}, q = {q}")
+            # save graph for logpdf vs mu to output file
+            if len(mus_) != 0: glogpdf = ROOT.TGraph(len(mus_), mus_, logpdf)
+            else:              glogpdf = ROOT.TGraph()            
+            fout.WriteObject(glogpdf, f"glogPDF_type0_Rang{rang}_{globalpi}")
 
-            # fit
-            if len(mus_)>0: pars = np.polyfit(mus_, pdflog, deg)
-            else          : pars = np.zeros(deg+1)
+            # define mu thresholds
+            mulow = q - 4.*np.sqrt(q)
+            muup  = q + 4.*np.sqrt(q)
+            gmuthrs[0].SetPoint(pi, q, mulow)
+            gmuthrs[1].SetPoint(pi, q, muup)
 
-            # flip the parameter positions (default ordering for fiTQun)
-            pars = np.flip(pars)
+            # Fit
+            fitfunc = get_fitting_function(mulow, muup)
+            if len(mus_) > 0: pars, _ = curve_fit(fitfunc, mus_, logpdf, p0=np.zeros(npar), method="trf")
+            else            : pars = np.zeros(npar)
 
             # save fitted parameters
             for par, gPar in zip(pars, gParams): gPar.SetPoint(pi, q, par)
-
-            # define mu thresholds (what are they used for?)
-            mulow = q - 4.*np.sqrt(q)
-            muup  = q + 4.*np.sqrt(q)
-            # if mulow<mus_[0] : mulow = mus_[0]
-            # if muup >mus_[-1]: muup  = mus_[-1]
-            gmuthrs[0].SetPoint(pi, q, mulow)
-            gmuthrs[1].SetPoint(pi, q, muup)
 
             # if (globalpi == 40):
             #     plt.ion()
@@ -122,7 +143,7 @@ def main():
             #     plt.figure()
             #     plt.title(f"q = {q}")
                 
-            #     plt.scatter(mus_, pdflog, color="k")
+            #     plt.scatter(mus_, logpdf, color="k")
             #     x = np.linspace(0, np.max(mus_), 1000)
             #     # x = x[(mulow<=x) & (x<=muup)]
             #     poly = np.poly1d(np.flip(pars))
@@ -145,7 +166,14 @@ def main():
         # save to output file
         for  pari, gPar   in enumerate(gParams): fout.WriteObject(  gPar, f"gParam_type0_Rang{rang}_{pari}")
         for bound, gmuthr in enumerate(gmuthrs): fout.WriteObject(gmuthr, f"gmuthr_type0_Rang{rang}_{bound}")
-            
+
+    # due to a bug in fiTQun, also save Rang = nqrange with empty values
+    gParams = [ROOT.TGraph() for par in range(npars[rang])]
+    gmuthrs = [ROOT.TGraph() for i in range(2)]
+    rang = nqranges
+    for  pari, gPar   in enumerate(gParams): fout.WriteObject(  gPar, f"gParam_type0_Rang{rang}_{pari}")
+    for bound, gmuthr in enumerate(gmuthrs): fout.WriteObject(gmuthr, f"gmuthr_type0_Rang{rang}_{bound}")
+
 
     # copy PUnhit to output file (TGraph copy not implemented in uproot)
     fin = ROOT.TFile(infilename)
