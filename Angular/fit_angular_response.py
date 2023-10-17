e238837a89b231e20e18fd00b2b5bd27c3e824d7
gondiaz <gdiazlop@cca014.in2p3.fr>
2023-10-17 15:16:59 +0200

    Add R range for fitting

--- Angular/fit_angular_response.py
+++ Angular/fit_angular_response.py
@@ -14,25 +14,29 @@ def main():
                                     , epilog      = """""")
     
     parser.add_argument("-v", "--verbose", action="store_true")
-    parser.add_argument("-i", "--infile", type=str, nargs="?", help = "", default="angular.root")
+    parser.add_argument("-i",  "--infile", type=str, nargs="?", help = "", default="angular.root")
+    parser.add_argument("-l",    "--lims", type=float, nargs=2, help="")
 
     parser.add_argument("polydeg", type=int, nargs=1, help = "")
 
     args = parser.parse_args()
     ##########################################
 
+    lims    = args.lims
     polydeg = args.polydeg[0]
 
     # read angular responses file
     with uproot.open(args.infile) as infile:
         # read angular response histogram computed from all the PMTs (side and caps)
         h, rbins, etabins = infile["AngularResponse_all"].to_numpy()
-        # select only histogram for the first bin in radious (closest to the PMT)
-        h = h[0]
+        # select 
+        il = np.digitize(lims[0], rbins)
+        iu = np.digitize(lims[1], rbins)-1
+        e = np.mean(h[il:iu, :], axis=0)
     
     # compute bin centers and fit angular response
     etas = (etabins[1:] + etabins[:-1])/2.
-    pars = np.polyfit(etas, h, polydeg)
+    pars = np.polyfit(etas, e, polydeg)
     
     # create polynomial TF1 and set the computed parameters
     tf1 = ROOT.TF1("angResp", f"pol{polydeg}", etabins[0], etabins[-1])
@@ -41,10 +45,10 @@ def main():
     if args.verbose:
         # plot results
         plt.figure(figsize=[5, 3])
-        plt.scatter(etas, h, color="k")
+        plt.scatter(etas, e, color="k")
         x = np.linspace(etabins[0], etabins[-1], 100)
         fitted = [tf1.Eval(eta) for eta in x]
-        plt.plot(x, fitted, color="k")
+        plt.plot(x, fitted, color="r")
         plt.xlabel(r"$\cos \eta$")
         plt.ylabel(r"$\epsilon(\cos \eta)$")
         plt.tight_layout()
