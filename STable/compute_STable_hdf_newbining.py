b55a03918ed220e9b75f32e2f68fcd4aa15de140
gondiaz <gdiazlop@cca020.in2p3.fr>
2024-02-19 17:07:46 +0100

    Add new bining to hdf STable

--- STable/compute_STable_hdf_newbining.py
+++ STable/compute_STable_hdf_newbining.py
@@ -13,63 +13,79 @@ from os.path   import expandvars, join, basename
 from STable.STable_tools import split_tubeids, clockwise_azimuth_angle, read_wcsim_geometry
 
 
-def process_table(infiles, tabname, bins, tubeids, vaxis, R, verbose):
+def read_photon_information(filename):
+
+    rootf = ROOT.TFile(filename)
+
+    tree = rootf.Get("sttree")
+    n = tree.GetEntries()
+
+    srcpos = np.zeros((n, 3), dtype=np.float32)
+    srcdir = np.zeros((n, 3), dtype=np.float32)
+    ihPMT  = np.zeros(n, dtype=int)
+    isct   = np.zeros(n, dtype=int)
+
+    for i in range(n):
+        tree.GetEvent(i)
+        srcpos[i] = np.frombuffer(tree.srcpos, dtype=np.float32)
+        srcdir[i] = np.frombuffer(tree.srcdir, dtype=np.float32)
+        ihPMT [i] = tree.ihPMT
+        isct  [i] = tree.isct
+    
+    rootf.Close()
+    return srcpos, srcdir, ihPMT, isct
+
+
+def process_table(infiles, tabname, bins, tubeids, R, mPMTmap, mPMTpositions, mPMTbins, verbose):
 
     if verbose: print(f">> Creating {tabname:6} table...")
 
     # Define scattering tables, notice the only change beside name and title is the use of R_PMT (bottom and top) and z_PMT (side)
     h_indirect = np.zeros(shape=tuple(  [len(bins_)-1 for bins_ in bins]))
-    h_direct   = np.zeros(shape=tuple([*[len(bins_)-1 for bins_ in bins[:3]], len(bins[-1])-1]))
+    h_direct   = np.zeros(shape=tuple([*[len(bins_)-1 for bins_ in bins[:3]], *[len(bins_)-1 for bins_ in bins[-2:]]]))
 
     # Loop over simulation files in order to fill the table histograms
     for n, filename in enumerate(infiles, 1):
         if verbose: print(f"  {(tabname):7}: Processing file {n}/{len(infiles)}...", basename(filename))
 
         # read data
-        with uproot.open(filename) as f:
-            try:
-                # photons reaching a PMT either directly or indirectly
-                # parent pos and dir
-                srcpos = f["sttree/srcpos"].array().to_numpy()
-                srcdir = f["sttree/srcdir"].array().to_numpy()
-                # PMT number/pos
-                ihPMT   = f["sttree/ihPMT"]  .array().to_numpy()
-                tubepos = f["sttree/tubepos"].array().to_numpy()
-                # ISTORY=(# of refl)*1000 + (# of scat); 0 if direct hit
-                isct = f["sttree/isct"].array().to_numpy()
-            except uproot.exceptions.KeyInFileError: continue
+        srcpos, srcdir, ihPMT, isct = read_photon_information(filename)
+
+        # select photons in tubeids
+        sel =  np.isin(ihPMT, tubeids)
+        srcpos = srcpos[sel]
+        srcdir = srcdir[sel]
+        ihPMT  = ihPMT [sel]
+        isct   = isct  [sel]
+
+        # get mPMT variables
+        mPMTinfo    = mPMTmap.loc[ihPMT, ("mPMTNo", "mPMT_PMTNo")].values.astype(int)
+        mPMTNos     = mPMTinfo[:, 0]
+        mPMT_PMTNos = mPMTinfo[:, 1]
+        mPMTpos     = mPMTpositions.loc[mPMTNos].values
+        mPMTzR      = mPMTbins     .loc[mPMTNos].values
 
         # Rotate with vertical direction given by vaxis
-        if vaxis != 2:
-            srcpos  = np.matmul(R,  srcpos.T).T
-            srcdir  = np.matmul(R,  srcdir.T).T
-            tubepos = np.matmul(R, tubepos.T).T
+        if R is not None:
+            srcpos  = np.matmul(R, srcpos.T).T
+            srcdir  = np.matmul(R, srcdir.T).T
 
         # Compute scattering table variables (transform from mm to cm)
-        zs    = 0.1*srcpos[:, 2]
-        Rs    = 0.1*np.sqrt(np.sum(srcpos[:, (0, 1)]**2, axis=1))
-        phi   = clockwise_azimuth_angle(tubepos, srcpos)
-        zd    = srcdir[:, 2]
-        theta = clockwise_azimuth_angle(srcdir, tubepos-srcpos)
-        # z_PMT used for side table, R_PMT used for bottom and top
-        z_PMT = 0.1*tubepos[:, 2]
-        R_PMT = 0.1*np.sqrt(np.sum(tubepos[:, (0, 1)]**2, axis=1))
-
-        # transform angles from [0, 2pi] to [-pi, pi]
-        phi  [  phi>np.pi] -= 2.*np.pi
-        theta[theta>np.pi] -= 2.*np.pi
-
-        # histograms
-        if tabname == "side": pmtpos = z_PMT
-        else                : pmtpos = R_PMT
+        zs      = 0.1*srcpos[:, 2]
+        Rs      = 0.1*np.sqrt(np.sum(srcpos[:, (0, 1)]**2, axis=1))
+        phi     = clockwise_azimuth_angle(mPMTpos, srcpos)
+        phi_dir = clockwise_azimuth_angle(srcdir, mPMTpos-srcpos)
+        z_dir   = srcdir[:, 2]
+        
         sel_indirect = (isct != 0)
         # indirect
-        sel  = sel_indirect & np.isin(ihPMT, tubeids)
-        h, _ = np.histogramdd((zs[sel], Rs[sel], phi[sel], zd[sel], theta[sel], pmtpos[sel]), bins=bins)
+        sel  = sel_indirect
+        h, _ = np.histogramdd((zs[sel], Rs[sel], phi[sel], phi_dir[sel], z_dir[sel], mPMTzR[sel], mPMT_PMTNos[sel]), bins=bins)
         h_indirect += h
+
         # direct
-        sel  = ~sel_indirect & np.isin(ihPMT, tubeids)
-        h, _ = np.histogramdd((zs[sel], Rs[sel], phi[sel], pmtpos[sel]), bins=(*bins[:3], bins[-1]))
+        sel  = ~sel_indirect
+        h, _ = np.histogramdd((zs[sel], Rs[sel], phi[sel], mPMTzR[sel], mPMT_PMTNos[sel]), bins=(*bins[:3], *bins[-2:]))
         h_direct += h
 
     # Add direction axis to direct light histogram
@@ -145,12 +161,12 @@ def main():
     # (bottom and top table) R_PMT
     # (side table) z_PMT: zposition of the PMT
     # define bounds
-    zs    = [-length/2., length/2.]
-    Rs    = [0., radius]
-    phi   = [-np.pi, +np.pi]
-    zd    = [-1., 1.]
-    theta = [-np.pi, +np.pi]
-    bounds = [zs, Rs, phi, zd, theta]
+    zs      = [-length/2., length/2.]
+    Rs      = [0., radius]
+    phi     = [0., 2.*np.pi]
+    phi_dir = [0., 2.*np.pi]
+    z_dir   = [-1, +1]
+    bounds  = [zs, Rs, phi, phi_dir, z_dir]
     # define bins
     bins = 5*[None]
     for dim, nbins in enumerate(args.nbins):
@@ -158,16 +174,39 @@ def main():
 
     # define PMT bins
     pmts_sel = pmts.set_index("TubeNo").loc[tubeids["side"]]
-    zPMTbinc = np.sort(pmts_sel.Position_x2.unique().astype(float))
+    side_mPMT_zs = pmts_sel.set_index("mPMT_PMTNo").loc[1].set_index("mPMTNo").loc[:, "Position_x2"].astype(float)
+    zPMTbinc = np.sort(side_mPMT_zs.unique())
     zPMTbins = (zPMTbinc[1:] + zPMTbinc[:-1])/2.
     zPMTbins = np.insert(zPMTbins,             0, zPMTbinc [0] - (zPMTbins [0]-zPMTbinc [0]))
     zPMTbins = np.insert(zPMTbins, len(zPMTbins), zPMTbinc[-1] - (zPMTbins[-1]-zPMTbinc[-1]))
 
-    pmts_sel = pmts.set_index("TubeNo").loc[tubeids["top"]] # assuming top and bottom positions are the same
-    RPMTbinc = np.sort(np.unique(np.sum(pmts_sel.loc[:, ("Position_x0", "Position_x1")]**2, axis=1)**0.5).astype(float))
+    pmts_sel = pmts.set_index("TubeNo").loc[tubeids["top"]]
+    top_mPMT_Rs = pmts_sel.groupby("mPMTNo").apply(lambda df: np.linalg.norm(df.set_index("mPMT_PMTNo").loc[1].to_frame().loc[["Position_x0", "Position_x1"]]))
+    pmts_sel = pmts.set_index("TubeNo").loc[tubeids["bottom"]]
+    bottom_mPMT_Rs = pmts_sel.groupby("mPMTNo").apply(lambda df: np.linalg.norm(df.set_index("mPMT_PMTNo").loc[1].to_frame().loc[["Position_x0", "Position_x1"]]))
+    # assuming top and bottom positions are the same
+    RPMTbinc = np.sort(bottom_mPMT_Rs.unique())
     RPMTbins = (RPMTbinc[1:] + RPMTbinc[:-1])/2.
     RPMTbins = np.insert(RPMTbins,             0, RPMTbinc [0] - (RPMTbins [0]-RPMTbinc [0]))
     RPMTbins = np.insert(RPMTbins, len(RPMTbins), RPMTbinc[-1] - (RPMTbins[-1]-RPMTbinc[-1]))
+
+    # define mPMT map (TubeNo->mPMTNo)
+    mPMTmap = pmts.set_index("TubeNo").loc[:, ("mPMTNo", "mPMT_PMTNo")]
+
+    # define mPMT_PMTNobins (implicitly assumes all modules contain the same number of PMTs)
+    mPMT_PMTNobinc = mPMTmap.set_index("mPMTNo").loc[1].mPMT_PMTNo.values.astype(int)
+    mPMT_PMTNobins = (mPMT_PMTNobinc[1:] + mPMT_PMTNobinc[:-1])/2.
+    mPMT_PMTNobins = np.insert(mPMT_PMTNobins, 0, mPMT_PMTNobinc[0] - (mPMT_PMTNobins[0]-mPMT_PMTNobinc[0]))
+    mPMT_PMTNobins = np.insert(mPMT_PMTNobins, len(mPMT_PMTNobins), mPMT_PMTNobinc[-1] - (mPMT_PMTNobins[-1]-mPMT_PMTNobinc[-1]))
+
+    # group the mPMT bins into a dictionary
+    mPMTbins = dict()
+    mPMTbins["side"]   = side_mPMT_zs
+    mPMTbins["top"]    = top_mPMT_Rs
+    mPMTbins["bottom"] = bottom_mPMT_Rs
+
+    # define mPMT positions as the central PMT position
+    mPMTpositions = pmts.set_index("mPMT_PMTNo").loc[1, ("mPMTNo", "Position_x0", "Position_x1", "Position_x2")].astype(float).set_index("mPMTNo")
 
     # compute scale factor for direct light 4D->6D (assumes uniform bin size) 
     # given by dx/Dx == nbins for the direction dimensions
@@ -184,6 +223,7 @@ def main():
         fout.create_carray(bins_group, f"bins_{dim}", atom, bins_.shape, obj=bins_, filters=filters)
     fout.create_carray(bins_group, "bins_zPMT", atom, zPMTbins.shape, obj=zPMTbins, filters=filters)
     fout.create_carray(bins_group, "bins_RPMT", atom, RPMTbins.shape, obj=RPMTbins, filters=filters)
+    fout.create_carray(bins_group, "bins_mPMT_PMTNo", atom, mPMT_PMTNobins.shape, obj=mPMT_PMTNobins, filters=filters)
     fout.flush()
     # define tables group tables
     tables_group = fout.create_group("/", "tables", "tables")
@@ -192,24 +232,28 @@ def main():
     results         = dict()
     future_to_table = dict()
 
-    # with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
-    #     for tabname in tabnames:
-    #         bins_ = bins.copy()
-    #         if tabname == "side": bins_.append(zPMTbins)
-    #         else                : bins_.append(RPMTbins)
-    #         future_to_table[executor.submit(process_table, infiles, tabname, bins_, tubeids[tabname], args.vaxis, R, args.verbose)] = tabname
+    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        for tabname in tabnames:
+            bins_ = bins.copy()
+            if tabname == "side": bins_.append(zPMTbins)
+            else                : bins_.append(RPMTbins)
+            bins_.append(mPMT_PMTNobins)
+            future_to_table[executor.submit(process_table, infiles, tabname, bins_, tubeids[tabname], R, mPMTmap, mPMTpositions, mPMTbins[tabname], args.verbose)] = tabname
 
-    #     # get results once all the tasks are finished
-    #     for future in concurrent.futures.as_completed(future_to_table):
-    #         h_indirect, h_direct = future.result()
-    #         tabname = future_to_table[future]
-    #         results[tabname] = (h_indirect, h_direct)
+        # get results once all the tasks are finished
+        for future in concurrent.futures.as_completed(future_to_table):
+            h_indirect, h_direct = future.result()
+            tabname = future_to_table[future]
+            results[tabname] = (h_indirect, h_direct)
 
     for tabname in tabnames:
-        bins_ = bins.copy()
-        if tabname == "side": bins_.append(zPMTbins)
-        else                : bins_.append(RPMTbins)
-        results[tabname] = process_table(infiles, tabname, bins_, tubeids[tabname], args.vaxis, R, args.verbose)
+        # bins_ = bins.copy()
+        # # add zRPMTbins
+        # if tabname == "side": bins_.append(zPMTbins)
+        # else                : bins_.append(RPMTbins)
+        # # add mPMT_PMTNo bins
+        # bins_.append(mPMT_PMTNobins)
+        # results[tabname] = process_table(infiles, tabname, bins_, tubeids[tabname], R, mPMTmap, mPMTpositions, mPMTbins[tabname], args.verbose)
 
         # Scattering tables
         if args.verbose: print(f">> Writting {tabname} table...")
