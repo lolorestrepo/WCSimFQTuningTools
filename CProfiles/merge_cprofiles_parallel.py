045bae18c3157f28e5afe19b61bd70424291fe28
gondiaz <gdiazlop@cca016.in2p3.fr>
2024-12-05 15:28:16 +0100

    Cosmetics and solve bug when pid is all

--- CProfiles/merge_cprofiles_parallel.py
+++ CProfiles/merge_cprofiles_parallel.py
@@ -1,21 +1,26 @@
-import os
+"""
+    merge_cprofiles_parallel.py
+"""
 import re
 import glob
 import argparse
 import itertools
-import uproot
-import ROOT
-import numpy  as np
-
 import multiprocessing
 import concurrent.futures
+
 from itertools import groupby
-from os.path   import expandvars, realpath, join, basename
+from os        import cpu_count
+from os.path   import expandvars, join, basename
+
+import uproot
+import ROOT
+import numpy as np
 
 
 def process_momentum(pid, momentum, files, htype, lock, verbose=False):
 
-    if verbose: print(f"Processing {momentum} MeV/c files for {pid}...".ljust(50))
+    if verbose: 
+        print(f"Processing {momentum} MeV/c files for {pid}...".ljust(50))
 
     nevents = 0
     # start with array to accumulate histogram entries
@@ -31,14 +36,11 @@ def process_momentum(pid, momentum, files, htype, lock, verbose=False):
             h_, _, _ = fin[f"{htype}g"].to_numpy()
             h += h_ # accumulate entries
 
-    # create, fill and save 2D histogram for this momentum
-    thbinw = thbins[1] - thbins[0]
-    sbinw =   sbins[1] -  sbins[0]
-
     th2d = ROOT.TH2D( f"g_{momentum}", f"g_{momentum}"
                     , len(thbins)-1, thbins
                     , len(sbins) -1, sbins)
-    for ix, iy in itertools.product(range(1, len(thbins)), range(1, len(sbins))): th2d.SetBinContent(ix, iy, h[ix-1, iy-1])
+    for ix, iy in itertools.product(range(1, len(thbins)), range(1, len(sbins))):
+        th2d.SetBinContent(ix, iy, h[ix-1, iy-1])
 
     with lock:
         fout = ROOT.TFile(f"cprofiles_{pid}_merged.root", "UPDATE")
@@ -54,35 +56,37 @@ def main():
     parser = argparse.ArgumentParser( prog        = "merge_cprofiles"
                                     , description = "description"
                                     , epilog      = "Text at the bottom of help")
-    
+
     parser.add_argument("-v", "--verbose", action="store_true")
 
     parser.add_argument( "indir", type=str, nargs=1, help = "directory containing produced files")
     parser.add_argument(   "pid", type=str,          help = "particle type (eg e-, mu+...", default="all")
     parser.add_argument(  "type", type=str, nargs=1, help = "type of cherenkov profile, true (tr) or weighted (wt)") # modify this, it can be tr/wt
-    
+
     args = parser.parse_args()
     ##########################################
 
-    # check pid and define output file 
+    # check pid and define output file
     if args.pid == "all":
-        if args.verbose: print("Merging all files")
-        output_file = "cprofiles_merged.root"
+        if args.verbose:
+            print("Merging all files in %s", args.indir[0])
+        output_file = "cprofiles_all_merged.root"
     elif args.pid in ["e-", "e+", "mu-", "mu+", "pi-", "pi+"]:
-        if args.verbose: print(f"Merging {args.pid} files")
+        if args.verbose: 
+            print(f"Merging {args.pid} files")
         output_file = f"cprofiles_{args.pid}_merged.root"
     else:
-        print(f"Unknown parameter: {args.pid}, should be e-, e+, mu-, mu+, pi-, pi+")
-        exit(1)
+        raise Exception(f"Unknown parameter: {args.pid}, should be e-, e+, mu-, mu+, pi-, pi+")
 
     # check if type is tr or wt
-    if args.type[0] not in ["tr", "wt"]: raise Exception("Type must be one of tr or wt")
+    if args.type[0] not in ["tr", "wt"]: 
+        raise Exception("Type must be one of tr or wt")
 
     # sorter function
     get_momentum_and_index = lambda fname: list(map(float, re.findall(r'\d+(?:\.\d+)?', basename(fname))[:2]))
 
     # get all files in input dir, filter out the '_flat.root' and sort them based on (momentum, index)
-    infiles = glob.glob(join(args.indir[0], "*"))
+    infiles = glob.glob(join(expandvars(args.indir[0]), "*"))
     if args.pid == "all":
         infiles = [f for f in infiles if re.match("out_([^_]+)_\d+(?:\.\d+)?_\d+.root", basename(f))]
     else:
@@ -101,7 +105,7 @@ def main():
     results = []
     with multiprocessing.Manager() as manager:
         lock = manager.Lock()
-        with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count()) as executor:
             future_to_momentum = {executor.submit(process_momentum, args.pid, momentum, files, args.type[0], lock, args.verbose): momentum for momentum, files in zip(momenta, groups)}
 
             # get results once all the tasks are finished
